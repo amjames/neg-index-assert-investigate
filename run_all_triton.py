84e460bbb63cd8e2a03594739c2131cc5f81a03d
Andrew M. James <andrew.m.james2@gmail.com>
2024-05-07 19:03:10 +0000

    fix runall

--- run_all_triton.py
+++ run_all_triton.py
@@ -32,7 +32,7 @@ from triton.compiler.compiler import AttrsDescriptor
 
 from torch._inductor.runtime import triton_helpers, triton_heuristics
 from torch._inductor.runtime.triton_helpers import libdevice, math as tl_math
-from torch._inductor.runtime.hints import AutotuneHint, ReductionHint, TileHint, instance_descriptor
+from torch._inductor.runtime.hints import AutotuneHint, ReductionHint, TileHint, instance_descriptor, DeviceProperties
 
 from torch._dynamo.testing import rand_strided
 from torch._C import _cuda_getCurrentRawStream as get_raw_stream
@@ -42,12 +42,11 @@ from torch._inductor.runtime.triton_heuristics import grid, split_scan_grid
 @triton_heuristics.pointwise(
     size_hints=[536870912], 
     filename=__file__,
-    triton_meta={'signature': {0: '*i64', 1: '*fp32', 2: '*i64', 3: '*fp32', 4: '*i64', 5: '*fp32', 6: '*i64', 7: '*fp32', 8: '*i64', 9: '*fp32', 10: '*fp32', 11: 'i32'}, 'device': 0, 'device_type': 'cuda', 'constants': {}, 'configs': [AttrsDescriptor(divisible_by_16=(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11), equal_to_1=())]},
-    inductor_meta={'autotune_hints': set(), 'kernel_name': 'triton_poi_fused_add_index_0', 'mutated_arg_names': [], 'no_x_dim': False, 'backend_hash': 'ce52ce89546c8ffd8a9cfa4405558e5c797ed169df3771129471a6e0b4baf65e', 'are_deterministic_algorithms_enabled': False, 'assert_indirect_indexing': True, 'autotune_local_cache': True, 'autotune_pointwise': True, 'autotune_remote_cache': False, 'dynamic_scale_rblock': True, 'max_autotune': False, 'max_autotune_pointwise': False, 'min_split_scan_rblock': 256, 'spill_threshold': 16, 'store_cubin': False, 'kernel_num_gb': 0.027303936},
+    triton_meta={'signature': {0: '*i64', 1: '*fp32', 2: '*i64', 3: '*fp32', 4: '*i64', 5: '*fp32', 6: '*i64', 7: '*fp32', 8: '*i64', 9: '*fp32', 10: '*fp32', 11: 'i32'}, 'device': DeviceProperties(type='cuda', index=0, cc=75, major=7, regs_per_multiprocessor=65536, max_threads_per_multi_processor=1024, multi_processor_count=30), 'constants': {}, 'configs': [AttrsDescriptor(divisible_by_16=(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11), equal_to_1=())]},
+    inductor_meta={'autotune_hints': set(), 'kernel_name': 'triton_poi_fused_add_index_0', 'mutated_arg_names': [], 'no_x_dim': False, 'backend_hash': 'ce52ce89546c8ffd8a9cfa4405558e5c797ed169df3771129471a6e0b4baf65e', 'are_deterministic_algorithms_enabled': False, 'assert_indirect_indexing': True, 'autotune_local_cache': True, 'autotune_pointwise': True, 'autotune_remote_cache': False, 'dynamic_scale_rblock': True, 'max_autotune': False, 'max_autotune_pointwise': False, 'min_split_scan_rblock': 256, 'spill_threshold': 16, 'store_cubin': False, 'kernel_num_gb': 2.819883008},
     min_elem_per_thread=0
 )
 @triton.jit
-def triton_(in_ptr0, in_ptr1, in_ptr2, in_ptr3, in_ptr4, in_ptr5, in_ptr6, in_ptr7, in_ptr8, in_ptr9, out_ptr0, xnumel, XBLOCK : tl.constexpr):
 def triton_(in_ptr0, in_ptr1, in_ptr2, in_ptr3, in_ptr4, in_ptr5, in_ptr6, in_ptr7, in_ptr8, in_ptr9, out_ptr0, xnumel, XBLOCK : tl.constexpr):
     xnumel = 536870912
     xoffset = tl.program_id(0) * XBLOCK
